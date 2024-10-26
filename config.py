62aee5a3fb15134e11b1a2d5de1f33e934cc32ce
sergei_vokhminov <rreaper@list.ru>
2024-10-26 19:50:53 +0300

    добавлены html шаблоны страниц, настройки bootstrap

---
+++ config.py
@@ -0,0 +1,16 @@
+#  Основные настройки
+#  Импорт библиотек
+import logging
+import os
+
+#  Корневая директория проекта
+ROOT_DIR = os.path.dirname(__file__)
+
+#  Путь до готового файла
+PATH_TO_FILE = os.path.join(ROOT_DIR, "html", "contacts.html")
+
+
+if __name__ == "__main__":
+    print(ROOT_DIR)
+    print()
+    print(PATH_TO_FILE)
